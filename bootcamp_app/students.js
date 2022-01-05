f0a928fc33ffa2dced643fdafef1437582c3b4c4
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-01-05 22:28:00 +0000

    Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,28 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort_name
+const queryString = `
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE ('${process.argv[2]}%')
-LIMIT ${process.argv[3]} ;
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit]
+
+pool.query(queryString, values)
 .then(res => { 
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
   })
 });
 
-// pool.query(`
-// SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort_name
-// FROM students
-// JOIN cohorts ON cohorts.id = students.cohort_id
-// WHERE cohorts.name LIKE (${process.argv[1]}%)
-// LIMIT ${process.argv[2]};
-// `)
-// .then(res => { 
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
-//   })
-// });
+
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.*) AS total_assistances
 FROM teachers 
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE ('${process.argv[2]}%')
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => { 
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
